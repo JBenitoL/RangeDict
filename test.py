22a6074b3fb1b3b9855f5fee1ff5836165351b07
jbenitoL <jbenitollorens@gmail.com>
2022-06-23 19:36:59 +0200

    test megative numbers

--- test.py
+++ test.py
@@ -2,7 +2,12 @@ import unittest
 from rangedict import RangeDic
 
 
-dict_test = {"a": "a", "b": "b", "<3":"Less than 3", ">=5": "Bigger or equal than 5",
+dict_test = {"a": "a", 
+            "b": "b", 
+            "<-3":"Less than 3", 
+            "[-2--1)":"Bigger than -2 (included) smaller than -1 (not included)",
+            ">=5": "Bigger or equal than 5",
+
                 "other_dict":{
                 "<=1": "Equal or smaller than 1",
                 "2-5": "Between 1 and 5 (1 included, 5 not included", 
@@ -20,10 +25,17 @@ class TestRangeDict(unittest.TestCase):
         self.assertEqual(range_dict["other_dict"]["(5-10)"], dict_test["other_dict"]["(5-10)"])
     
     def test_first_level_less_than(self):
-        self.assertEqual(range_dict[2.999], dict_test["<3"])
-        self.assertEqual(range_dict[2], dict_test["<3"])
-        self.assertEqual(range_dict[-5], dict_test["<3"])
-        self.assertEqual(range_dict[-10], dict_test["<3"])
+        self.assertNotEqual(range_dict[-3.00], dict_test["<-3"])
+        self.assertEqual(range_dict[-3.001], dict_test["<-3"])
+        self.assertEqual(range_dict[-3.5], dict_test["<-3"])
+        self.assertEqual(range_dict[-5], dict_test["<-3"])
+        self.assertEqual(range_dict[-10], dict_test["<-3"])
+
+    def test_first_level_first_range(self):
+        self.assertNotEqual(range_dict[-3.1], dict_test["[-2--1)"])
+        self.assertEqual(range_dict[-2], dict_test["[-2--1)"])
+        self.assertEqual(range_dict[-1.5], dict_test["[-2--1)"])
+        self.assertNotEqual(range_dict[-1], dict_test["[-2--1)"])
 
     def test_first_level_bigger_or_equal_than(self):
         self.assertEqual(range_dict[5], dict_test[">=5"])
